5f6c98b7c6991f198401fda95ad98fbd841fc48f
Amanda Shih <pandafulmanda@gmail.com>
2025-01-27 10:41:01 -0600

    switch to svg map and d3 as opposed to leaflet, less distracting

--- assets/main.js
+++ assets/main.js
@@ -4,15 +4,15 @@
     const counties = await countiesPromise;
     const formTXT = await formDataPromise;
 
-    const [headerRow, ...formRows] = formTXT.split("\r\n");
+    const newLine = new RegExp("\r?\n")
+    const [headerRow, ...formRows] = formTXT.split(newLine);
 
     const headers = headerRow.split("\t");
     const entries = formRows
       .map(function (formRow) {
-        return formRow.split("\t");
+        return formRow.split("\t").map(function (value) { return value.replaceAll('"', '').trim()});
       })
       .filter(function (formRowSplit) {
-        console.log({ formRowSplit })
         return formRowSplit.length === headers.length;
       })
       .map(function (formRowSplit) {
@@ -24,7 +24,7 @@
 
         return rowData;
       })
-    console.log({ entries, formTXT, headerRow, formRows, headers })
+
     const formData = Object.groupBy(
       entries,
       function (formData) {
@@ -32,11 +32,16 @@
       }
     );
 
+    const entryCounts = Object.values(formData).map(function (item) { return item.length })
+
+    const maxEntries = entryCounts.length > 0 && Math.max.apply(Math, entryCounts) || 1
+
     const tableDataHTML = entries.map(function (entry) {
       return `
         <tr data-county="${ entry["Location"] }">
           <td>${ entry["Date of the incident"] }</td>
           <td>${ entry["Location"] }</td>
+          <td>${ formData[entry["Location"]].length }</td>
           <td>${ [entry["Law Enforcement Agencies"],  entry["Law Enforcement Agencies: Other"]].filter(function (item) { return item.length > 0}).join(', ') }</td>
           <td>${ entry["Did An Arrest Occur?"] }</td>
         </tr>
@@ -65,8 +70,15 @@
       };
     });
 
-    return counties;
+    return {
+      geojson: counties,
+      meta: {
+        maxEntries,
+        minEntries: 0,
+      }
+    };
   }
+
   function highlightData(props) {
     const entries = document.querySelectorAll(`#incident-data tr[data-county="${props.name}"]`)
 
@@ -74,9 +86,7 @@
       entry.classList.add('selected')
     })
 
-    if (entries && entries[0]) {
-      console.log(entries[0])
-      
+    if (entries && entries[0]) {      
       document.querySelector('.dt-scroll-body').scrollTo({
         top: entries[0].offsetTop,
         left: 0,
@@ -92,131 +102,73 @@
       entry.classList.remove('selected')
     })
   }
-  const map = L.map("map").setView([31.9686, -99.9018], 6);
 
-  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 14,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+  function drawMap(mapInfo) {
+    const data = mapInfo.geojson
+    const meta = mapInfo.meta
 
-  // control that shows state info on hover
-  const info = L.control();
+    const backgroundColor = "#EAF2FA"
+    const landColor = "#FFEDA0"
+    const landStroke = "#800026"
 
-  info.onAdd = function (map) {
-    this._div = L.DomUtil.create("div", "info");
-    this.update();
-    return this._div;
-  };
+    const mapEl = document.querySelector('#map')
 
-  info.update = function (props) {
-    const contents = props
-      ? `<b>${props.entryCount}</b>`
-      : "Hover over a county";
-    this._div.innerHTML = `<h4>Number of incidents</h4>${contents}`;
-  };
+    const chartWidth = mapEl.clientWidth
+    const chartHeight = mapEl.clientHeight
 
-  info.addTo(map);
+    const scale = d3.scaleLinear([meta.minEntries, meta.maxEntries], [landColor, landStroke])
 
-  // get color depending on population density value
-  function getColor(d) {
-    return d > 5
-      ? "#800026"
-      : d > 3
-      ? "#FC4E2A"
-      : d > 0
-      ? "#FD8D3C"
-      : "#FFEDA0";
+    const projection = d3.geoMercator()
+      .scale([2000])
+      .center([-99.9018, 31.4686])
+      .translate([chartWidth / 2, chartHeight / 2]);
+
+    const pathGenerator = d3.geoPath(projection);
+
+    const svg = d3.create('svg')
+      .attr("viewBox", [0, 0, chartWidth, chartHeight])
+      .attr("title", "Map")
+      .attr('width', chartWidth)
+      .attr('height', chartHeight)
+
+    svg.append("rect")
+      .attr("class", "background")
+      .attr("width", chartWidth)
+      .attr("height", chartHeight)
+      .attr('fill', backgroundColor);
+
+    svg.selectAll('g.counties path')
+      .data(data.features.filter(d => d.geometry.type === "MultiPolygon"))
+      .join('path')
+        .attr('d', pathGenerator)
+        .attr('fill', function (d) {
+          return scale(d.properties.entryCount)
+        })
+        .attr('stroke', landStroke)
+        .attr('stroke-width', 1)
+      .on('mouseenter', function (mouseEvent, d) {
+        highlightData(d.properties)
+      })
+      .on('mouseout', function (mouseEvent, d) {
+        unhighlightData();
+      });
+
+    d3.select('#map')
+      .node()
+      .appendChild(svg.node())
   }
 
-  function style(feature) {
-    return {
-      weight: 2,
-      opacity: 1,
-      color: "white",
-      dashArray: "1",
-      fillOpacity: 0.7,
-      fillColor: getColor(feature.properties.entryCount),
-    };
-  }
+  Promise.all([
+    fetch("./data/tx-counties.geojson"),
+    // fetch("./data/filtered.tsv")
+    fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQe6otdyhVEVKf744PAhGz9UNtQ1ELrfTHkyEUrQvkTEh37xsGIjLg7HU5q1Lpcpp4cqNvXpSL_2q4e/pub?output=tsv")
+  ])
+  .then(function (responses) {
+    const counties = responses[0].json();
+    const formData = responses[1].text();
+    return [counties, formData];
+  })
+  .then(handleCountiesLoaded)
+  .then(drawMap);
 
-  function highlightFeature(e) {
-    const layer = e.target;
-
-    layer.setStyle({
-      weight: 2,
-      color: "#666",
-      dashArray: "",
-      fillOpacity: 0.7,
-    });
-
-    layer.bringToFront();
-
-    info.update(layer.feature.properties);
-
-    highlightData(layer.feature.properties);
-  }
-
-  Promise.all([fetch("./data/tx-counties.geojson"), fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQe6otdyhVEVKf744PAhGz9UNtQ1ELrfTHkyEUrQvkTEh37xsGIjLg7HU5q1Lpcpp4cqNvXpSL_2q4e/pub?output=tsv")])
-    .then(function (responses) {
-      const counties = responses[0].json();
-      const formData = responses[1].text();
-      return [counties, formData];
-    })
-    .then(handleCountiesLoaded)
-    .then(function (countiesGeo) {
-      window.geojson = L.geoJson(countiesGeo, {
-        style,
-        onEachFeature,
-      }).addTo(map);
-    });
-
-  /* global statesData */
-
-  function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-
-    unhighlightData();
-  }
-
-  function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-  }
-
-  function onEachFeature(feature, layer) {
-    layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: zoomToFeature,
-    });
-  }
-
-  // map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
-  const legend = L.control({ position: "bottomright" });
-
-  legend.onAdd = function (map) {
-    const div = L.DomUtil.create("div", "info legend");
-    // const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
-    const grades = [0, 3, 5];
-    const labels = [];
-    let from, to;
-
-    for (let i = 0; i < grades.length; i++) {
-      from = grades[i];
-      to = grades[i + 1];
-
-      labels.push(
-        `<i style="background:${getColor(from + 1)}"></i> ${from}${
-          to ? `&ndash;${to}` : "+"
-        }`
-      );
-    }
-
-    div.innerHTML = labels.join("<br>");
-    return div;
-  };
-
-  legend.addTo(map);
 })();
